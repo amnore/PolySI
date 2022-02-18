0035b4cd6762bb49c5c4abafa787dd837dff84ec
Chen Zhenge <me@markle.one>
2022-02-18 12:55:56 +0800

    Implement SISolver

--- src/main/java/verifier/SIVerifier.java
+++ src/main/java/verifier/SIVerifier.java
@@ -1,18 +1,36 @@
 package verifier;
 
 import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.graph.EndpointPair;
+import com.google.common.graph.MutableNetwork;
+import com.google.common.graph.Network;
+import com.google.common.graph.NetworkBuilder;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 
 import graph.PrecedenceGraph;
 import graph.TxnNode;
+import io.grpc.netty.shaded.io.netty.util.internal.shaded.org.jctools.queues.MessagePassingQueue.Consumer;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import util.Pair;
+import monosat.Graph;
+import monosat.Lit;
+import monosat.Logic;
+import monosat.Solver;
 import util.UnimplementedError;
 import util.VeriConstants;
 
 public class SIVerifier extends AbstractLogVerifier {
 	// precedence graph constructed from logs
-	private final PrecedenceGraph graph;
+	final PrecedenceGraph graph;
 
 	public SIVerifier(String logfd) {
 		super(logfd);
@@ -22,17 +40,18 @@ public class SIVerifier extends AbstractLogVerifier {
 	@Override
 	public boolean audit() {
 		var constraints = generateConstraints(graph);
-		throw new UnimplementedError();
+		var solver = new SISolver(graph, constraints);
+		return solver.solve();
 	}
 
 	@Override
 	public int[] count() {
-		throw new Error("Not supported");
+		throw new UnimplementedError();
 	}
 
 	@Override
 	public boolean continueslyAudit() {
-		throw new Error("Not supported");
+		throw new UnimplementedError();
 	}
 
 	/*
@@ -69,12 +88,11 @@ public class SIVerifier extends AbstractLogVerifier {
 	 *
 	 * @return the set of constraints generated
 	 *
-	 * For each triple of transactions A, B, C
-	 * such that B reads A and C writes the key B read,
-	 * generate polygraph for the following two conditions:
+	 * For each triple of transactions A, B, C such that B reads A and C writes the
+	 * key B read, generate polygraph for the following two conditions:
 	 *
-	 * 1. C precedes A, then C ->(ww) A ->(wr) B
-	 * 2. A precedes C, then A ->(wr) B ->(rw) C, A ->(ww) C
+	 * 1. C precedes A, then C ->(ww) A ->(wr) B 2. A precedes C, then A ->(wr) B
+	 * ->(rw) C, A ->(ww) C
 	 *
 	 * Note that it is possible for C to precede B in SI.
 	 */
@@ -83,26 +101,17 @@ public class SIVerifier extends AbstractLogVerifier {
 		var readOps = graph.m_readFromMapping;
 		var constraints = new HashSet<SIConstraint>();
 
-		for (var a: graph.allNodes()) {
-			for (var op: a.getOps()) {
+		for (var a : graph.allNodes()) {
+			for (var op : a.getOps()) {
 				if (op.isRead) {
 					continue;
 				}
 
-				for (var bop: readOps.get(op.id)) {
+				for (var bop : readOps.get(op.id)) {
 					var b = graph.getNode(bop.txnid);
-					for (var c: writeTxns.get(op.key_hash)) {
-						constraints.add(new SIConstraint(
-							new SIEdge[]{
-								new SIEdge(c, a, EdgeType.WW),
-								new SIEdge(a, b, EdgeType.WR)
-							},
-							new SIEdge[]{
-								new SIEdge(a, b, EdgeType.WR),
-								new SIEdge(b, c, EdgeType.RW),
-								new SIEdge(a, c, EdgeType.WW)
-							}
-						));
+					for (var c : writeTxns.get(op.key_hash)) {
+						constraints.add(new SIConstraint(new SIEdge[] { new SIEdge(c, a, EdgeType.WW), },
+								new SIEdge[] { new SIEdge(b, c, EdgeType.RW), new SIEdge(a, c, EdgeType.WW) }));
 					}
 				}
 			}
@@ -114,8 +123,8 @@ public class SIVerifier extends AbstractLogVerifier {
 	// get the set of txns that write to each key
 	private static Map<Long, Set<TxnNode>> getKeyAndWriteTxns(PrecedenceGraph graph) {
 		var m = new HashMap<Long, Set<TxnNode>>();
-		for (var t: graph.allNodes()) {
-			for (var op: t.getOps()) {
+		for (var t : graph.allNodes()) {
+			for (var op : t.getOps()) {
 				if (op.isRead) {
 					continue;
 				}
@@ -128,6 +137,95 @@ public class SIVerifier extends AbstractLogVerifier {
 		}
 
 		return m;
+	}
+}
+
+class SISolver {
+	final Solver solver = new Solver();
+	final Graph graph = new Graph(solver);
+	final HashSet<Lit> lits = new HashSet<>();
+	final HashBiMap<TxnNode, Integer> nodeMap = HashBiMap.create();
+
+	/*
+	 * Construct SISolver from constraints
+	 *
+	 * First construct two graphs:
+	 *
+	 * 1. Graph A contains WR and WW edges.
+	 *
+	 * 2. Graph B contains RW edges.
+	 *
+	 * Each edge is associated with a literal. The edge exists in the graph iff. the
+	 * literal is true.
+	 *
+	 * 1. For WR edges, the literal is Lit.True
+	 *
+	 * 2. For WW and RW edges, the literal is created from the constraints. For edge
+	 * sets S1 and S2 in a same constraint, the literal of S1 is the negation of
+	 * S2's.
+	 *
+	 * Then, we construct a third graph C using these two: If P -> Q in A and Q -> R
+	 * in B, then P -> R in C The literal of P -> R is the conjunction of their
+	 * literals.
+	 *
+	 * Lastly, we add graph A and C to monosat and assert this graph is acyclic.
+	 */
+	SISolver(PrecedenceGraph precedenceGraph, Set<SIConstraint> constraints) {
+		Supplier<MutableNetwork<TxnNode, Lit>> newGraph = () -> NetworkBuilder.directed().allowsParallelEdges(true)
+				.build();
+		var graphA = newGraph.get();
+		var graphB = newGraph.get();
+		var graphC = newGraph.get();
+
+		precedenceGraph.allNodes().forEach(n -> {
+			graphA.addNode(n);
+			graphB.addNode(n);
+			graphC.addNode(n);
+			nodeMap.put(n, graph.addNode());
+		});
+
+		// add WR edges
+		precedenceGraph.allEdges().forEach(e -> graphA.addEdge(e.source(), e.target(), Lit.True));
+
+		// add WW and RW edges
+		constraints.forEach(c -> {
+			BiConsumer<Lit, SIEdge[]> addEdges = (lit, edges) -> {
+				Arrays.stream(edges).forEach(e -> {
+					var graph = e.type == EdgeType.WW ? graphA : graphB;
+					graph.addEdge(e.from, e.to, lit);
+				});
+			};
+
+			var lit = new Lit(solver);
+			addEdges.accept(lit, c.edges1);
+			addEdges.accept(Logic.not(lit), c.edges2);
+		});
+
+		// construct graphC
+		precedenceGraph.allNodes().forEach(n -> {
+			var pred = graphA.predecessors(n);
+			var succ = graphB.successors(n);
+
+			pred.forEach(p -> succ.forEach(s -> {
+				var predEdges = graphA.edgesConnecting(p, n);
+				var succEdges = graphA.edgesConnecting(n, s);
+
+				predEdges.forEach(e1 -> succEdges.forEach(e2 -> graphC.addEdge(p, s, Logic.and(e1, e2))));
+			}));
+		});
+
+		Consumer<Network<TxnNode, Lit>> addGraph = g -> g.nodes()
+				.forEach(n -> g.successors(n).forEach(s -> g.edgesConnecting(n, s)
+						.forEach(e -> solver.assertEqual(e, graph.addEdge(nodeMap.get(n), nodeMap.get(s))))));
+
+		addGraph.accept(graphA);
+		addGraph.accept(graphC);
+
+		solver.assertTrue(graph.acyclic());
+	}
+
+	boolean solve() {
+		return solver.solve();
 	}
 }
 
