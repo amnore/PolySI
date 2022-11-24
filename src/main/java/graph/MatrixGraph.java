69736d3d37fd3c4c16558a848e9fafd914ea524f
Zhenge Chen <me@markle.one>
2022-11-24 10:34:45 +0800

    Revert "Use run-length format for reachability"
    This reverts commit 33835bc95e60cd426119d1ac1daa76fdd2e468a9.

--- src/main/java/graph/MatrixGraph.java
+++ src/main/java/graph/MatrixGraph.java
@@ -94,7 +94,6 @@ public class MatrixGraph<T> implements MutableGraph<T> {
                 assert topoOrder.indexOf(j) > i;
                 result.set(n, j);
                 result.adjacency[n].or(result.adjacency[j]);
-                result.adjacency[n].runOptimize();
                 // for (var k = 0; k < adjacency[0].length; k++) {
                 // result.adjacency[n][k] |= result.adjacency[j][k];
                 // }
